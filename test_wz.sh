248b92235e7c9f2ce80c9a3b8567ddbe0471f0b4
tky5622 <task5622@gmail.com>
2023-09-22 22:33:01 +0900

    edit file setting for vsd

--- test_wz.sh
+++ test_wz.sh
@@ -1,6 +1,6 @@
 # magic123
 CUDA_VISIBLE_DEVICES=1 python launch.py --config configs/magic123-coarse-sd.yaml --train data.image_path=load/images/luffy_medal_rgba.png system.prompt_processor.prompt="a high-resolution DSLR image of Luffy medal" 
-CUDA_VISIBLE_DEVICES=1 python launch.py --config configs/magic123-refine-sd.yaml --train data.image_path=load/images/luffy_medal_rgba.png system.prompt_processor.prompt="a high-resolution DSLR image of Luffy medal" system.geometry_convert_from=outputs/magic123-coarse-sd/luffy_medal_rgba.png-a_high-resolution_DSLR_image_of_Luffy_medal@20230908-102845/ckpts/last.ckpt system.renderer.context_type=cuda
+CUDA_VISIBLE_DEVICES=1 python launch.py --config configs/magic123-refine-sd.yaml --train data.image_path=load/images/luffy_medal_rgba.png system.prompt_processor.prompt="a high-resolution DSLR image of Luffy medal" system.geometry_convert_from=outputs/magic123-coarse-sd/luffy_medal_rgba.png-a_high-resolution_DSLR_image_of_Luffy_medal@20230922-032430/ckpts/last.ckpt system.renderer.context_type=cuda
 # zero123
 CUDA_VISIBLE_DEVICES=1 python launch.py --config configs/zero123.yaml --train data.image_path=load/images/luffy_medal_rgba.png system.guidance.pretrained_model_name_or_path=/home/asset/threestudio/load/zero123/zero123-xl.ckpt
 
@@ -10,3 +10,5 @@ CUDA_VISIBLE_DEVICES=1 python launch.py --config configs/prolificdreamer-patch.y
 CUDA_VISIBLE_DEVICES=1 python launch.py --config configs/prolificdreamer-patch.yaml --train system.prompt_processor.prompt="a high-resolution DSLR image of gold flat thin Luffy medal"
 # magic123 with multiview
 CUDA_VISIBLE_DEVICES=1 python launch.py --config configs/magic123-coarse-sd_multiview.yaml --train system.prompt_processor.prompt="a high-resolution DSLR image of Luffy medal" 
+# magic123 with vsd loss
+CUDA_VISIBLE_DEVICES=1 python launch.py --config configs/magic123-coarse-sd_VSD.yaml --train data.image_path=load/images/luffy_medal_rgba.png system.prompt_processor.prompt="a high-resolution DSLR image of Luffy medal" 
